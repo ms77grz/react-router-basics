65dcd040ed621352b9b4bbef7647f59e3c79df37
Magomed Shamaev <ms77grz@gmail.com>
2022-05-13 11:00:36 +0300

    Shared Layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
-function App() {
-  return <h2>React Router 6 Tutorial</h2>;
-}
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Products from './pages/Products';
+import Error from './pages/Error';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}>
+          <Route path='about' element={<About />} />
+          <Route path='products' element={<Products />} />
+          <Route path='*' element={<Error />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
